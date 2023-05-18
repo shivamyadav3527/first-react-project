4dac9bfeea477e41f6adff8e586bf7c41cde483e
Shivam <shivamyadav3527@gmail.com>
2023-05-18 15:36:25 +0530

    first project on react

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+import logo from '../logo.svg'
+function Navbar(){
+    return(
+        <nav>
+            <img 
+                src={logo} />
+            <h2>REACTS FACTS</h2>
+            <h3>REACT COURSE - PROJECT 1</h3>
+        </nav>
+    )
+}
+export default Navbar;
